c414c7a3290b35d66c68c82fd4ceb9011ffb9723
David Long <davdaarn@users.noreply.github.com>
2021-03-03 10:54:54 -0700

    added feature component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import { HEROES } from "../mock-heroes";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heros = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
